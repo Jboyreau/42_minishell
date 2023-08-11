cfcc9c5586df448280a450fa9fe34c9e2b79b1ee
JBoyreau <jacques.boyreau94@gmail.com>
2023-08-11 19:38:46 +0200

    first commit

---
+++ lexer/lexer_3.c
@@ -0,0 +1,74 @@
+#include "minishell.h"
+
+#define SUCCESS 0
+#define FAILURE 1
+
+static void	ch_type(t_leaf *tr, char *str, int len, int count)
+{
+	if (count == 0)
+		return (fill_leaf(tr, CMD, len, str), (void)0);
+	if ((*(tr - 1)).type == CMD || (*(tr - 1)).type == ARG)
+		fill_leaf(tr, ARG, len, str);
+	else if ((*(tr - 1)).type == R_QUOTE || (*(tr - 1)).type == DR_QUOTE)
+		fill_leaf(tr, FIL, len, str);
+	else if ((*(tr - 1)).type > NL && (*(tr - 1)).type <= OR)
+		fill_leaf(tr, CMD, len, str);
+	else if ((*(tr - 1)).type == DL_QUOTE)
+		fill_leaf(tr, LIM, len, str);
+}
+
+static void	fnormal_string(t_leaf *tr, char *str, int *i, int *count)
+{
+	int s;
+	static int bidon = 0;
+
+	s = *i;
+	while (*(str + (*i)))
+	{
+		if (*(str + (*i)) == ' ')
+			return (ch_type(tr, str + s, *i-s, *count), ++(*count), (void)0);
+		if (char_is_token(*(str + (*i)), *(str + (*i) + 1), &bidon) == SUCCESS)
+			return (ch_type(tr, str + s, *i-s, *count), ++(*count), (void)0);
+		++(*i);
+	}
+	return (ch_type(tr, str + s, *i-s, *count), ++(*count), (void)0);
+}
+
+static void	fdq_sequence(t_leaf *tr, char *str, int *i, int *count)
+{
+	int	j;
+
+	j = *i;
+	while (*(str + j))
+		++j;
+	while (*(str + j) != '\"')
+		--j;
+	if (*i == j)
+		return (fnormal_string(tr, str, i, count));	
+	ch_type(tr, str + (*i) + 1, j - ((*i) + 1), *count);
+	return (*i = j + 1, ++(*count), (void)0);
+}
+
+static void	fq_sequence(t_leaf *tr, char *str, int *i, int *count)
+{
+	int	j;
+
+	j = *i;
+	while (*(str + j))
+		++j;
+	while (*(str + j) != '\'')
+		--j;
+	if (*i == j)
+		return (fnormal_string(tr, str, i, count));
+	ch_type(tr, str + (*i) + 1, j - ((*i) + 1), *count);
+	return (*i = j + 1, ++(*count), (void)0);
+}
+
+void	flongest_token(t_leaf *tr, char *str, int *i, int *count)
+{
+	if (*(str + (*i)) == '\'')
+		return (fq_sequence(tr, str, i, count));
+	if (*(str + (*i)) == '\"')
+		return (fdq_sequence(tr, str, i, count));
+	return (fnormal_string(tr, str, i, count));
+}
---
+++ lexer/minishell.h
@@ -0,0 +1,112 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*	Token types:
+	{
+		Indentifiers	:
+			files
+			variables
+			commandes arg0 arg1 arg2 ... arg n
+
+		Punctuations	:
+			\n
+			)
+
+		Keywords		:
+			'		(dans une séquence entre guillemets)
+			"		(dans une séquence entre guillemets, ne marche pas pour $)
+
+		Operators		:
+			(	unary
+					string)
+			$	unary			(substite l'operand et l'operateur par une valeur)
+					string
+					?		(fait référence à une variable contenant le statut de sortie de la derniere commande)
+			=	binary			(affecte une valeur à une variable)
+					variable = value
+			<	binary/unary	(input redirection)
+					commande < file
+					< file (sans commande spécifié, lit stdin à partir de file sans rien faire d'autre)
+			>	binary			(output redirection)
+					commande > file
+			>>  binary			(output redirection append)
+					commande >> file
+			<<	binary/unary	(input redirection and heredoc)
+					commande << LIMITER
+					<< LIMITER (sans commande spécifié, lit stdin à partir d'stdin sans rine faire d'autre)
+			|	binary
+					cmd | cmd
+			*	binary/unary	(se substitue par tous les noms de fichiers du dossier courant, la commande est dupliquée autant de fois qu'il y a une correspondance entre les fichiers du dossier et l'operand)
+					commande *string
+					commande ...*...
+					*string
+					...*...
+			&&	binary		(go yt)
+					cmd && cmd
+			||	binary		(go yt)
+					cmd || cmd
+
+		Constantes		:
+			string	"Alice" ou Alice
+			tableau	("rouge" "vert" "bleu")
+	}
+*/
+
+enum types
+{
+//Indentifiers:
+	ARG,			// ARGUMENTS		0
+	FIL,			// FILE				1
+	CMD,			// COMMANDE			2
+	LIM,			// LIMITER HEREDOC	3
+//Punctuations:
+	CLS_PAR,		// )				4
+	NL,				// \n				5
+//Operators:
+	OP_PAR,			// (				6
+	L_QUOTE,		// <				7
+	R_QUOTE,		// >				8
+	DL_QUOTE,		// <<				9
+	DR_QUOTE,		// >>				10
+	PIPE,			// |				11
+	AND,			// &&				12
+	OR,				// ||				13
+//Constantes:
+	STR,			// STRING == ARG d'export
+	ARR,			// tableau == ARG d'export
+/*******************************************************************************/
+//Dans les strings:	(invisible pour le shell à l'exterieur d'une string)		/
+	DOLLAR,			// $														/
+	EQUAL,			// =														/
+	QUOTE,			// ' (inside a string)										/
+	D_D_QUOTE,		// " (inside a string)										/
+	STAR,			// *														/
+	VAR,			// VARIABLE													/
+/*******************************************************************************/
+};
+
+typedef struct s_leaf
+{
+	char	type;
+	int		len;
+	char	*word;
+	struct s_tree	*dad;
+	struct s_tree	*r;
+	struct s_tree	*l;
+} t_leaf;
+
+typedef struct s_commands
+{
+	char	*str;
+	t_leaf	*tr;
+	int		count;
+} t_cmd;
+
+char	char_is_token(char c0, char c1, int *i);
+char	fchar_is_token(t_leaf *tr, char *str, int *i);
+void	longest_token(char *str, int *i, int *count);
+void	flongest_token(t_leaf *tr, char *str, int *i, int *count);
+void	fill_leaf(t_leaf *tr, char type, int len, char *word);
+t_cmd	*lexer(t_cmd *hll);
+
+#endif
